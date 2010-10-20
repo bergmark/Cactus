1dec6fd77fb0b551062a2a5052182d6ea190a846
Raevel <github.com@edea.se>
2010-10-20 02:03:43 +0200

    Added Util.JSON and Util.Options modules. Options probably has a few bugs.

---
+++ test/Util.JSON.test.js
@@ -0,0 +1,12 @@
+require('../CactusJuice.js');
+
+module.exports = (function () {
+  var JSON = CactusJuice.Util.JSON;
+  return {
+    test : function (assert) {
+      assert.eql('"aoeu"', JSON.stringify("aoeu"));
+      assert.eql("1", JSON.stringify(1));
+      assert.eql('"[1,2,\'3\']"', JSON.stringify("[1,2,'3']"));
+    }
+  };
+})();
