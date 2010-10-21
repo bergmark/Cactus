494bda73fc1f78591ed601cc2c61287fb7fa29a9
Raevel <github.com@edea.se>
2010-10-21 16:12:28 +0200

    Revert "Options rewrite started, allows for the built in primitive types now."
    This reverts commit c1b43ee736baf7be8abd224fba9d1aa19d17e944.

--- test/Util.Options.test.js
+++ test/Util.Options.test.js
@@ -16,17 +16,11 @@ module.exports = (function () {
       assert.eql("aoeu", o.parse("aoeu"));
       exception(/expected string, but got number/i, o.parse.bind(o, 1));
       exception(/expected string, but got boolean/i, o.parse.bind(o, true));
-
       o = new Options({
         type : "number"
       });
       assert.eql(100, o.parse(100));
       exception(/expected number, but got string/i, o.parse.bind(o, "1"));
-
-      o = new Options({
-        type : ["number"]
-      });
-      assert.eql(JSON.stringify([1, 2]), JSON.stringify(o.parse([1, 2])));
     }
   };
 })();
