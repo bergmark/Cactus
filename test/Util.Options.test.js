5241c74ed55062a2160740f80459cb241135f7ac
Raevel <github.com@edea.se>
2010-10-21 17:08:53 +0200

    Added arrays, nested arrays are also supported.

--- test/Util.Options.test.js
+++ test/Util.Options.test.js
@@ -7,6 +7,10 @@ module.exports = (function () {
   var JSON = CactusJuice.Util.JSON;
   var stringify = JSON.stringify;
 
+  var jsoneq = function (assert, a, b) {
+    return assert.eql(JSON.stringify(a), JSON.stringify(b));
+  };
+
   return {
     a : function (assert) {
       var exception = assertException.curry(assert);
@@ -16,11 +20,27 @@ module.exports = (function () {
       assert.eql("aoeu", o.parse("aoeu"));
       exception(/expected string, but got number/i, o.parse.bind(o, 1));
       exception(/expected string, but got boolean/i, o.parse.bind(o, true));
+
       o = new Options({
         type : "number"
       });
       assert.eql(100, o.parse(100));
       exception(/expected number, but got string/i, o.parse.bind(o, "1"));
+
+      o = new Options({
+        type : [{ type : "number"}]
+      });
+      jsoneq(assert, [1, 2], o.parse([1, 2]));
+      exception(/expected \{"type":\[\{"type":"number"\}\]\}, but got string/i, o.parse.bind(o, "a"));
+      exception(/error in property "0.": expected number, but got string/i, o.parse.bind(o, ["a"]));
+      exception(/error in property "1.": expected number, but got boolean/i, o.parse.bind(o, [1, true]));
+      exception(/error in property "0.": expected number, but got string[\s\S]+error in property "1.": expected number, but got boolean/i, o.parse.bind(o, ["a", true]));
+
+      // Nesting of arrays.
+      o = new Options({
+        type : [{ type : [{ type : "number" }] }]
+      });
+      jsoneq(assert, [[1,2]], o.parse([[1,2]]));
     }
   };
 })();
