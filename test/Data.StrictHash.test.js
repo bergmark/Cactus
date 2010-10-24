b02a38f0c6d66a1b0e2492d9c9cf78603fcdaa30
Raevel <github.com@edea.se>
2010-10-24 13:40:43 +0200

    StrictHash: No longer takes a hash arg.

--- test/Data.StrictHash.test.js
+++ test/Data.StrictHash.test.js
@@ -5,7 +5,7 @@ module.exports = (function () {
   var assertException = CactusJuice.Dev.Assertion.exception;
   return {
     StrictHash : function (assert) {
-      var sh = new StrictHash({ keys : ["x", "y"] });
+      var sh = new StrictHash(["x", "y"]);
       assert.ok(!("define" in sh));
       sh.set("x", 1);
       assert.eql(1, sh.get("x"));
@@ -13,7 +13,7 @@ module.exports = (function () {
       assertException(assert, /StrictHash:.+?undefined/i, sh.set.bind(sh, "z"));
     },
     map : function (assert) {
-      var sh = new StrictHash({ keys : ["x", "y"] });
+      var sh = new StrictHash(["x", "y"]);
       sh.set("x", 1);
       sh.set("y", 2);
       sh = sh.map(function (v) {
