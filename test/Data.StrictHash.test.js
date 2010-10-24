b7755fdf6b4435340d85777b787268127379e24d
Raevel <github.com@edea.se>
2010-10-24 13:21:38 +0200

    StrictHash: implemented map.

--- test/Data.StrictHash.test.js
+++ test/Data.StrictHash.test.js
@@ -11,6 +11,16 @@ module.exports = (function () {
       assert.eql(1, sh.get("x"));
       sh.set("y", 2);
       assertException(assert, /StrictHash:.+?undefined/i, sh.set.bind(sh, "z"));
+    },
+    map : function (assert) {
+      var sh = new StrictHash({ keys : ["x", "y"] });
+      sh.set("x", 1);
+      sh.set("y", 2);
+      sh = sh.map(function (v) {
+        return v * 10;
+      });
+      assert.eql(10, sh.get("x"));
+      assert.eql(20, sh.get("y"));
     }
   };
 })();
