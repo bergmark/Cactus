4867497b7ab3a589cc5a14ea89699983cd86e647
Adam Bergmark <adam@edea.se>
2011-03-15 19:07:33 +0100

    Enumerable: Added select test.

--- test/Data.Enumerable.test.js
+++ test/Data.Enumerable.test.js
@@ -1,6 +1,8 @@
 module.exports = (function () {
   var Enumerable = CactusJuice.Data.Enumerable;
   var Collection = CactusJuice.Data.Collection;
+  var CMath = CactusJuice.Addon.Math;
+
   Class("Coll", {
     does : Enumerable,
     has : {
@@ -23,6 +25,9 @@ module.exports = (function () {
       },
       map : function (f) {
         return this.fromArray(Collection.map(this.toArray(), f));
+      },
+      select : function (f) {
+        return this.fromArray(Collection.select(this.toArray(), f));
       }
     }
   });
@@ -30,6 +35,9 @@ module.exports = (function () {
   return {
     map : function (f) {
       assert.strictEqual("2,3,4", new Coll({ arr : [1,2,3] }).map(function (v) { return v + 1; }).toArray().join(","));
+    },
+    select : function (f) {
+      assert.strictEqual("1,3", new Coll({ arr : [1,2,3] }).select(CMath.odd.bind(CMath)).toArray().join(","));
     }
   };
 })();
