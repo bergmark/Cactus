56d5e734e52dd3f7194450aa2531b5ce9a89a182
Raevel <github.com@edea.se>
2011-04-03 07:17:55 +0200

    Util.CollectionAggregate: Added stub.

--- Cactus.js
+++ Cactus.js
@@ -31,3 +31,4 @@ require('./lib/Data/ArrayControllerDecorator');
 
 require('./lib/Data/Money');
 require('./lib/Util/IdentityMap');
+require('./lib/Util/CollectionAggregate');
---
+++ test/Util.CollectionAggregate.js
@@ -0,0 +1,22 @@
+require("../Cactus");
+var assert = require("assert");
+var CollectionAggregate = Cactus.Util.CollectionAggregate;
+module.exports = {
+  a : function () {
+    Class("C2", {
+    });
+    CollectionAggregate.add(C2, "x");
+    var c = new C2();
+    assert.ok("addX" in c);
+    assert.ok("removeX" in c);
+    assert.ok("hasX" in c);
+    assert.ok("xCount" in c);
+
+    c.addX(1);
+    assert.ok(c.hasX(1));
+    assert.ok(!c.hasX(2));
+    assert.strictEqual(1, c.xCount());
+    c.removeX(1);
+    assert.ok(!c.hasX(1));
+  }
+};
