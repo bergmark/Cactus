bba724d701be6b1b77b2cdda00040f80a22bd0f5
Raevel <github.com@edea.se>
2011-04-29 10:41:22 +0200

    Added Data and getWithDefault (no predefined defaults yet).

--- test/Application.FormHelper2.test.js
+++ test/Application.FormHelper2.test.js
@@ -2,13 +2,17 @@ var FormHelper = Cactus.Application.FormHelper2;
 var C = Cactus.Data.Collection;
 var Renderer = Cactus.Application.Renderer;
 
+var jsoneq = function (a, b) {
+  return assert.strictEqual(JSON.stringify(a), JSON.stringify(b));
+};
+
 var userfh = new FormHelper({
   action : "/new",
   fields : {
-    email : null,
-    name : null,
-    password : null,
-    passwordConfirmation : null
+    email : { type : "string" },
+    name : { type : "string" },
+    password : { type : "string" },
+    passwordConfirmation : { type : "string", required : false }
   }
 });
 
@@ -17,5 +21,44 @@ module.exports = {
     var fh = userfh;
     assert.eql(["email", "name", "password", "passwordConfirmation"], fh.getFieldNames());
     assert.strictEqual("/new", fh.getAction());
+  },
+  populate : function () {
+    var fh = userfh;
+    var data = fh.newData();
+    data.populate({
+      name : "test",
+      email : "test@example.com",
+      password : "pass",
+      passwordConfirmation : "pass"
+    });
+    jsoneq({
+      name : "test",
+      email : "test@example.com",
+      password : "pass",
+      passwordConfirmation : "pass"
+    }, data.get());
+
+    // Can't get if required values are missing.
+    data = fh.newData();
+    data.populate({});
+    assert.throws(data.get.bind(data), function (e) {
+      assert.ok(/"name": Missing property/.test(e.message));
+      assert.ok(/"password": Missing property/.test(e.message));
+      assert.ok(/"email": Missing property/.test(e.message));
+      assert.ok(/"name": Missing property/.test(e.message));
+      assert.ok(!/"passwordConfirmation"/.test(e.message));
+      return true;
+    });
+
+    // Getting values on validation errors (for view).
+    data = fh.newData();
+    data.populate({
+      name : "test",
+      passwordConfirmation : "pass"
+    });
+    var gwd = data.getWithDefault.bind(data);
+    assert.strictEqual("test", gwd("name", "foo"));
+    assert.strictEqual("pass", gwd("passwordConfirmation", "bar"));
+    assert.strictEqual("baz", gwd("password", "baz"));
   }
 };
