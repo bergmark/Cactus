8b45e7a4b28135921a4cd368f642beda2241f5e7
Raevel <github.com@edea.se>
2011-05-27 16:43:15 +0200

    ValueTransformer properties are now named forward/backward.

--- test/Util.ValueTransformer.test.js
+++ test/Util.ValueTransformer.test.js
@@ -5,20 +5,20 @@ module.exports = {
   },
   transform : function () {
     var intToString = new ValueTransformer({
-      transform : function (v) {
+      forward : function (v) {
         return "" + v;
       },
-      reverse : function (v) {
+      backward : function (v) {
         return parseInt(v, 10);
       }
     });
-    assert.strictEqual("1", intToString.transform(1));
-    assert.strictEqual(1, intToString.reverse("1"));
+    assert.strictEqual("1", intToString.forward(1));
+    assert.strictEqual(1, intToString.backward("1"));
   },
   "allow omission of transformers" : function () {
     var idTransformer = new ValueTransformer();
-    equal(1, idTransformer.transform(1));
-    equal(1, idTransformer.reverse(1));
+    equal(1, idTransformer.forward(1));
+    equal(1, idTransformer.backward(1));
   },
   "getting the transformers" : function () {
     function toString() {
@@ -28,10 +28,10 @@ module.exports = {
       return parseInt(v, 10);
     }
     var intToString = new ValueTransformer({
-      transform : toString,
-      reverse : toInt
+      forward : toString,
+      backward : toInt
     });
-    equal(toString, intToString.getTransform());
-    equal(toInt, intToString.getReverse());
+    equal(toString, intToString.getForward());
+    equal(toInt, intToString.getBackward());
   }
 };
