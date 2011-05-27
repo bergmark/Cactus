0934c37eb88bcaa21a3ca33926720400c1d21098
Raevel <github.com@edea.se>
2011-05-27 18:16:28 +0200

    KeyValueCoding:hasKeyPath now always returns a bool.

--- test.js
+++ test.js
@@ -17,8 +17,8 @@ global.contEx = function (cont, reg) {
     this.CONTINUE();
   });
 };
-global.ok = function (v, msg) { assert.ok(v, msg); };
-global.not = function (v, msg) { assert.ok(v === false, msg); };
+global.ok = function (v, msg) { true.should.equal(v, msg); };
+global.not = function (v, msg) { false.should.equal(v, msg); };
 global.equal = function (a, b, msg) { assert.strictEqual(a, b, msg); };
 global.notequal = function (a, b, msg) { assert.notStrictEqual(a, b, msg); };
 global.eql = function (a, b, msg) { a.should.eql(b, msg); };
